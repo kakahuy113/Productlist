8d648ef79fe6a2b9285e0d6ec45f017e90cc814b
kakahuy113 <kakahuy113@gmail.com>
2019-08-29 16:35:04 +0700

    fix detail-product(name)

--- src/app/detail-product/detail-product.component.ts
+++ src/app/detail-product/detail-product.component.ts
@@ -7,12 +7,12 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class DetailProductComponent implements OnInit {
   @Input() product;
-  /* name: string = 'Leaf Rake';
+ /*   name: string = 'Leaf Rake';
   code : string = 'GDN-0011';
   
   availability: string = 'March 19,2016';
   Price : string = '$19.85';
-  star: string = '3.2'; */
+  star: string = '3.2';  */
   Description: string = 'Leaf rake with 48-inch wooden handle';
  
   constructor() { }
